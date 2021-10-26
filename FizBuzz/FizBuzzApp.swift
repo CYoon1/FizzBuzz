4c91841eccb378ebfce23abab2ed801235e4795d
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2021-10-25 21:39:45 -0400

    Initial Commit

---
+++ FizBuzz/FizBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizBuzzApp.swift
+//  FizBuzz
+//
+//  Created by Christopher Yoon on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct FizBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
